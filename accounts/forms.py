aa74f6e86e350d2e252fab6af685d5c682fdfc4a
kloi8 <152299798+kloi8@users.noreply.github.com>
2024-04-09 18:23:03 +0300

    D13.4

---
+++ accounts/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from allauth.account.forms import SignupForm
+from django.core.mail import mail_admins
+from django.core.mail import EmailMultiAlternatives #инициализатор класса в который передается текст сообщения
+from django.contrib.auth.models import Group
+from django.core.mail import send_mail
+
+
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="Email")
+    first_name = forms.CharField(label="Имя")
+    last_name = forms.CharField(label="Фамилия")
+
+class Meta:
+    model = User
+    fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "password1",
+        "password2",
+    )
+
+class CustomSignupForm(SignupForm):
+    def save(self, request):
+        user = super().save(request)
+
+        subject = 'Добро пожаловать на наш новостной портал!'
+        text = f'{user.username}, вы успешно зарегистрировались на сайте!'
+        html = (
+            f'<b>{user.username}</b>, вы успешно зарегистрировались на '
+            f'<a href="http://127.0.0.1:8000/news">сайте</a>!'
+        )
+        msg = EmailMultiAlternatives(
+            subject=subject, body=text, from_email=None, to=[user.email]
+        )
+
+        msg.attach_alternative(html, "text/html")
+        msg.send()
+
+        mail_admins(
+            subject='Новый пользователь!',
+            message=f'Пользователь {user.username} зарегистрировался на сайте.'
+        )
+
+        authors_users = Group.objects.get(name="authors")
+        user.groups.add(authors_users) #это удалили тк добавили рассылку на почту
+        # -# send_mail( - - - это убрали потому что добавили  хтмл текст
+        # -#     subject='Добро пожаловать на наш новостной портал!',
+        # -#     message=f'{user.username}, вы успешно зарегистрировались!',
+        # -#     from_email=None,  # будет использовано значение DEFAULT_FROM_EMAIL
+        # -#     recipient_list=[user.email],
+        # -# )
+        return user
